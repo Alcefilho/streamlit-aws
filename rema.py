05815137edc60a6770388cdb78f057bccf976b1e
Alcebiades Filho <alcebiadesabf@hotmail.com>
2021-06-03 13:46:01 -0300

    run

--- rema.py
+++ rema.py
@@ -1,15 +1,19 @@
 import numpy as np
 import pandas as pd
-#import matplotlib.pyplot as plt
+import time
+import math
 import streamlit as st 
-import streamlit_wordcloud as wordcloud
 from collections import namedtuple
 import altair as alt
 import math
 import pandas as pd
 import streamlit as st
-from wordcloud import WordCloud, STOPWORDS
+import streamlit as st
+import streamlit as st
+import base64
 
+#x = st.slider('x')  
+#st.write(x, 'squared is', x * x)
 #############################################################################################
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  Usuario   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<#
 #############################################################################################
@@ -43,35 +47,134 @@ from wordcloud import WordCloud, STOPWORDS
 #iii) Deverão deixar claro quais são as unidades de entrada para cada parâmetro e quais são as
 #unidades de saída de seus resultados. Eliminando desta forma compreensões erradas;
 
+def download_link(object_to_download, download_filename, download_link_text):
+    if isinstance(object_to_download,pd.DataFrame):
+        object_to_download = object_to_download.to_csv(index=False)
+    # some strings <-> bytes conversions necessary here
+    b64 = base64.b64encode(object_to_download.encode()).decode()
+    return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
 
-st.write(f"""
-# Bem Vindo ao REMA
-## Programa para ajudar o constutor cívil tomar decisões 
-""")
-user_input = st.text_input("label goes here")
-st.write(f"""
-:fire: :fire: 
-""")
-st.info("""
-        :point_left: **To get started, choose a demo on the left sidebar.**
-    """)
-name = st.text_input('Name')
-if not name:
-    st.warning('Please input a name.')
-    st.stop()
-    st.success('Thank you for inputting a name.')
+def alert():
+    return(st.warning('Não aceitamos valores em branco ou negativos'))
 
-
-import streamlit as st
-
-add_selectbox = st.sidebar.selectbox(
-    "O que você quer averiguar?",
-    ("Quem olhou meu perfil?", "Quais são meus amigos?", "Que profissão predomina meus amigos?")
+name = st.sidebar.selectbox(
+    'Conversor de unidades',
+    ('Unidades de Força e Pressão', 'Unidades de Comprimento')
 )
 
+if name == "Unidades de Comprimento":
+    if st.sidebar.button('Metros para Milíetros'):
+        number = st.sidebar.number_input('Insira o valor em Metros')
+        st.continue()
+        st.sidebar.warning(f'''{number*1000} Milímetros''')
+    if st.sidebar.button('Milímetros para Metros'):
+        number2 = st.sidebar.number_input('Insira o valor em Metros')
+        st.sidebar.warning(f'''{number2/1000} Metros''')
+        
+if name == "Unidades de Força e Pressão":
+    if st.sidebar.button('Newtons/metro² para Pascal'):
+        number3 = st.sidebar.number_input('Insira o valor em N/m²')
+        st.sidebar.warning(f'''{number3*1} Pa''')
+    if st.sidebar.button('GigaPascal para Pascal'):
+        number4 = st.sidebar.number_input('Insira o valor em GigaPascal')
+        st.sidebar.warning(f'''{number4*1000000000} Pascal''') 
+    if st.sidebar.button('MegaPascal para Pascal'):
+        number5 = st.sidebar.number_input('Insira o valor em MegaPascal')
+        st.sidebar.warning(f'''{number5*1000000} Pascal''')           
+            
+    
+#if st.sidebar.button('Código fonte'):
+     
+expander = st.sidebar.beta_expander("Autores do REMA")
+expander.write(""" **Alcebiades Alves Barbosa Filho** \n
+               Engenharia Física Mat: 201402442""")
+expander.write("""**Ana Paula Moura Saran** \n 
+               Engenharia de Computação Mat:201703703""")
+expander.write(""" **Luiz Yokoyama Felix de Souza ** \n
+                Engenharia de Computação Mat: 201602428""")
+st.write(f"""
+# Bem Vindo ao REMA
+## Programa para ajudar o constutor civíl tomar decisões 
+""")
 
-#with st.beta_container():
-#    st.write("This is inside the container")
-#   # You can call any Streamlit command, including custom components:
-#    st.bar_chart(np.random.randn(50, 3))
-#st.write("This is outside the container")
+    
+carga_dist = st.number_input('Insira o valor (em Newton/metro) da carga distribuída w')
+if not carga_dist or carga_dist < 0:
+    alert()
+    st.stop()
+
+escoamento = st.number_input('Por favor insira o valor (em Pascal) do limite de escoamento dos arames')
+if not escoamento or escoamento < 0:
+    alert()
+    st.stop()
+
+
+elasticidade = st.number_input('Por favor insira o valor (em Pascal) do Módulo de elasticidade dos arames')
+if not elasticidade or elasticidade < 0:
+    alert()
+    st.stop()
+for i in range(1):
+    a = st.number_input("Por favor insira o valor (em metros) da distância entre os pontos AB")
+    if not a or a < 0:
+        alert()
+        st.stop()
+    b = st.number_input("Por favor insira o valor (em metros) da distância entre os pontos BC")
+    if not b or b < 0:
+        alert()
+        st.stop()
+    c = st.number_input("Por favor insira o valor (em metros) da distância entre os pontos CG")
+    if not c or c < 0:
+        alert()
+        st.stop()
+    
+for i in range(1):
+    e = st.number_input("Por favor insira o valor (em metros) da distância entre os pontos EB")
+    if e < 0 or not e:
+        alert()
+        st.stop()    
+    d = st.number_input("Por favor insira o valor (em metros) da distância entre os pontos DC")
+    if not d or d < 0 or d > e:
+        st.warning('Lembrando que o comprimento DC tem que ser menor ou igual o comprimento EB')
+        alert()
+        st.stop()    
+        
+ag = a+b+c; E = elasticidade; A = math.pi*d**2/4; L_ab = a; L_ag = ag; L_eb = e
+W = carga_dist; Fc = ((L_eb*W**2/2) - L_ab*A*E*(d-e))/(a*d)+(e*(a+b)); Fb = (A*E*(d-e)+(d*Fc))/e; Fa = (W*L_ag-Fb-Fc)
+temp_c = Fc/A;temp_b = Fb/A
+
+my_bar = st.progress(0)
+
+for percent_complete in range(100):
+    time.sleep(0.001)
+    my_bar.progress(percent_complete + 1)
+    
+if temp_c > E or temp_b > E :
+    st.warning("O arame ultrapassou o limite tente novamente")
+    st.stop()
+else:
+    st.warning("O arame não ultrapassou o limite tente")  
+
+D_b=(Fb*L_eb)/(A*E); D_c=(Fc*d)/(A*E); D_g=(D_c*L_ag)/(a+b); E_eb=D_b/e;E_dc=D_c/d
+
+st.write(f'''### Os deslocamentos dos pontos B, C e G são:
+B = **{round(D_b,2)}** m C = **{round(D_c,2)}** m G = **{round(D_g,2)}** m \n
+---
+### As deformações dos arames de apoio são: 
+Arame EB = **{round(E_eb,2)}** sem unidades Arame DC D = **{round(E_dc,2)}** sem unidades \n
+---
+### As reações de apoio são:
+No ponto A:**{round(Fa,2)}** N/m E:**{round(Fb,2)}** N/m D:**{round(Fc,2)}** N/m
+''')
+   
+output = f'''\t RESULTADO DO RELATÓRIO DO REMA \n
+Os deslocamentos dos pontos B, C e G são:
+B = {round(D_b,2)} m C = {round(D_c,2)} m G = {round(D_g,2)} m \n
+As deformações dos arames de apoio são: 
+Arame EB = {round(E_eb,2)} sem unidades Arame DC D = {round(E_dc,2)} sem unidades \n
+As reações de apoio são:
+No ponto A = {round(Fa,2)} N/m E = {round(Fb,2)} N/m D = {round(Fc,2)} N/m
+'''
+
+if st.button('Download'):
+    tmp_download_link = download_link(output, 'Resultado.txt', 'Clique aqui para baixar o resultado dessa análise em txt')
+    st.markdown(tmp_download_link, unsafe_allow_html=True)
