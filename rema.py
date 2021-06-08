66f3cb20e1781051ba2c439a8affdaa9f7672576
Alcebiades Filho <alcebiadesabf@hotmail.com>
2021-06-08 18:15:23 -0300

    dsds

--- rema.py
+++ rema.py
@@ -118,7 +118,7 @@ for i in range(1):
         alert()
         st.stop()
 
-    escoamento = st.number_input('Por favor insira o valor (em MegaPascal) do limite de escoamento dos arames',step=.001, format="%1.3f")
+    escoamento = st.number_input('Por favor insira o valor (em MegaPascal) do limite de escoamento dos arames',step=0.001, format="%1.5f")
     if not escoamento or escoamento < 0:
         alert()
         st.stop()
