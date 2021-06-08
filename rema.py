5aa2bee1ca341da35448437d486d059b8f4fdc8b
Alcebiades Filho <alcebiadesabf@hotmail.com>
2021-06-08 17:13:31 -0300

    update-luiz
    Update feito pelo luiz

--- rema.py
+++ rema.py
@@ -5,11 +5,6 @@ import math
 import streamlit as st 
 from collections import namedtuple
 import altair as alt
-import math
-import pandas as pd
-import streamlit as st
-import streamlit as st
-import streamlit as st
 import base64
 
 #############################################################################################
@@ -67,7 +62,7 @@ name = st.sidebar.selectbox(
 )
              
 if name == "Unidades de Comprimento":
-    if st.sidebar.checkbox('Metros para Milíetros'):
+    if st.sidebar.checkbox('Metros para Milímetros'):
         number1 = st.sidebar.number_input('Insira o valor em Metros')
         st.sidebar.write(f''' **{number1*1000}** Milímetros''')
     if st.sidebar.checkbox('Milímetros para Metros'):
@@ -106,28 +101,28 @@ expander.write(""" **Luiz Yokoyama Felix de Souza ** \n
                 Engenharia de Computação Mat: 201602428""")
 st.write(f"""
 # Bem Vindo ao REMA
-## Programa para ajudar o constutor civíl tomar decisões 
+## Programa para ajudar o constutor civil tomar decisões 
 """)
 
 st.write('''![fig](https://figrema.s3.amazonaws.com/rema-fig.jpg)''')
 
 for i in range(1):
-    diam = st.number_input("Por favor insira o valor (em metros) do diâmetro dos arames",step=1e-5,format="%.5f")
+    diam = st.number_input("Por favor insira o valor (em metros) do diâmetro dos arames",step=1e-4,format="%.5f")
     if not diam or diam < 0:
         alert()
         st.stop() 
              
-    carga_dist = st.number_input('Insira o valor (em KN/metro) da carga distribuída w',step=1)
+    carga_dist = st.number_input('Insira o valor (em KN/metro) da carga distribuída w', step=1)
     if not carga_dist or carga_dist < 0:
         alert()
         st.stop()
 
-    escoamento = st.number_input('Por favor insira o valor (em MegaPascal) do limite de escoamento dos arames')
+    escoamento = st.number_input('Por favor insira o valor (em MegaPascal) do limite de escoamento dos arames', step=10)
     if not escoamento or escoamento < 0:
         alert()
         st.stop()
          
-    elasticidade = st.number_input('Por favor insira o valor (em GigaPascal) do Módulo de elasticidade dos arames')
+    elasticidade = st.number_input('Por favor insira o valor (em GigaPascal) do Módulo de elasticidade dos arames', step=10)
     if not elasticidade or elasticidade < 0:
         alert()
         st.stop()
@@ -147,14 +142,14 @@ for i in range(1):
         alert()
         st.stop()
         
-    e = st.number_input("Por favor insira o valor (em metros) da distância entre os pontos EB")
+    e = st.number_input("Por favor insira o valor (em metros) da distância entre os pontos EB", step=1e-4,format="%.5f")
     if e < 0 or not e:
         alert()
         st.stop() 
            
-    d = st.number_input("Por favor insira o valor (em metros) da distância entre os pontos DC")
+    d = st.number_input("Por favor insira o valor (em metros) da distância entre os pontos DC", step=1e-4,format="%.5f")
     if not d or d < 0 or d > e:
-        st.warning('Lembrando que o comprimento DC tem que ser menor ou igual ao comprimento EB')
+        st.warning('Lembrando que o comprimento DC tem que ser menor ou igual ao comprimento EB, com a barra partindo de uma pequena inclinação!')
         alert()
         st.stop()  
        
@@ -164,10 +159,18 @@ escoamento = escoamento*1000000 # Mega
 elasticidade = elasticidade*1000000000 # Giga
 
 
-        
-ag = a+b+c; E = elasticidade; A = math.pi*diam**2/4; L_ab = a; L_ag = ag; L_eb = e
 
-W = carga_dist; Fc = (((L_eb*W*(L_ag**2))/2) - (L_ab*A*E*(d-e)))/(a*d)+(e*(a+b)); Fb = (A*E*(d-e)+(d*Fc))/e; Fa = (W*L_ag-Fb-Fc)
+ag = a+b+c; E = elasticidade; A = math.pi*diam**2/4; L_ab = a; L_ag = ag; L_eb = e; L_ac = a+b; L_dc = d
+
+W_fornecido = carga_dist; 
+Dc = L_ac*(L_eb-L_dc)/(L_ac-L_ab)
+Db = L_ab*Dc/L_ac
+Db = abs(Db)
+Dc = abs(Dc)
+Fb = Db*A*E/L_eb
+Fc = Dc*A*E/L_dc
+W = (L_ab*Fb+L_ac*Fc)/(L_ag/2*L_ag)
+Fa = -Fb-Fc+W*L_ag
 
 temp_c = Fc/A; temp_b = Fb/A
 
@@ -190,32 +193,42 @@ if temp_b > E :
     st.stop()
         
 else:
-    st.warning("Os arames não ultrapassaram o limite de escoamento")  
+    st.warning("Os arames não ultrapassaram o limite de escoamento.")  
 
-D_b=(Fb*L_eb)/(A*E); D_c=(Fc*d)/(A*E); D_g=(D_c*L_ag)/(a+b); E_eb=D_b/e;E_dc=D_c/d
+Dg=(Dc*L_ag)/(L_ac); E_eb=Db/L_eb; E_dc=Dc/L_dc
+W_dif = W - W_fornecido
 
 st.write(f'''### Os deslocamentos dos pontos B, C e G são:
-* Ponto B = **{round(D_b,5)}** m 
-* Ponto C = **{round(D_c,5)}** m 
-* Ponto G = **{round(D_g,5)}** m \n
+* Ponto B = **{round(Db,5)}** m 
+* Ponto C = **{round(Dc,5)}** m 
+* Ponto G = **{round(Dg,5)}** m \n
 ---
 ### As deformações dos arames de apoio são: 
-* Arame EB = **{round(E_eb,5)}** sem unidades  
-* Arame DC = **{round(E_dc,5)}** sem unidades \n
+* Arame EB = **{round(E_eb,5)}** admensional  
+* Arame DC = **{round(E_dc,5)}** admensional \n
 ---
 ### As reações de apoio são:
-* No ponto A = **{abs(round(Fa,2))}** N 
-* No ponto E = **{abs(round(Fb,2))}** N 
-* No ponto D = **{abs(round(Fc,2))}** N
+* No ponto A = **{abs(round(Fa,5))}** N 
+* No ponto E = **{abs(round(Fb,5))}** N 
+* No ponto D = **{abs(round(Fc,5))}** N \n
+---
+### Com as medidas de arame fornecidas, a carga w necessária para a estrutura ficar na horizontal seria:
+* w = **{abs(round(W,5))}** N/m 
+* Para o w fornecido, existe uma diferença de = **{round(W_dif,5)}** N/m 
+### Você pode modificar as medidas até obter o resultado desejado.
 ''')
+
    
 output = f'''\t RESULTADO DO RELATÓRIO DO REMA \n
 Os deslocamentos dos pontos B, C e G são:
-B = {round(D_b,5)} m C = {round(D_c,5)} m G = {round(D_g,5)} m \n
+B = {round(Db,5)} m C = {round(Dc,5)} m G = {round(Dg,5)} m \n
 As deformações dos arames de apoio são: 
-Arame EB = {round(E_eb,5)} sem unidades Arame DC D = {round(E_dc,5)} sem unidades \n
+Arame EB = {round(E_eb,5)} admensional Arame DC D = {round(E_dc,5)} admensional \n
 As reações de apoio são:
-No ponto A = {abs(round(Fa,2))} N E = {abs(round(Fb,2))} N D = {abs(round(Fc,2))} N
+No ponto A = {abs(round(Fa,2))} N E = {abs(round(Fb,2))} N D = {abs(round(Fc,2))} N \n
+Com as medidas de arame fornecidas, a carga w necessária para a estrutura ficar na horizontal seria:
+w = {abs(round(W,5))} N/m 
+Para o w fornecido, existe uma diferença de {round(W_dif,5)} N/m 
 '''
 
 if st.button('Download'):
